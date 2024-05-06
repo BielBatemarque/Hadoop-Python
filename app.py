b666abfe7ca9b2db677d1636679fb1d79ae1a948
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-05-06 19:52:18 -0300

    Fix: entradas do projeto

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from mrjob.job import MRJob
-from src.mapper import MRWordFrequencyCountMapper
-from src.reducer import MRWordFrequencyCountReducer
+import re
 
 class MRWordFrequencyCount(MRJob):
 
-    def steps(self):
-        return [
-            self.mr(mapper=MRWordFrequencyCountMapper,
-                    reducer=MRWordFrequencyCountReducer)
-        ]
+    def mapper(self, _, line):
+        # Usa expressão regular para encontrar todas as palavras na linha
+        for word in re.findall(r'\w+', line):
+            # Emite cada palavra com contagem 1
+            yield word.lower(), 1
+
+    def reducer(self, key, values):
+        # Soma as contagens de cada palavra
+        yield key, sum(values)
 
 if __name__ == "__main__":
     MRWordFrequencyCount().run()
--- src/reducer.py
+++
@@ -1,7 +0,0 @@
-from mrjob.job import MRJob
-
-class MRWordFrequencyCountReducer(MRJob):
-
-    def reducer(self, key, values):
-        # Soma as contagens de cada palavra
-        yield key, sum(values)
